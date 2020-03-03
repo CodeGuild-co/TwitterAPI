b8ed6d110566034d02a659dc3ea67f1802213488
VilliersAPI2020 <61736827+VilliersAPI2020@users.noreply.github.com>
2020-03-03 14:20:37 +0000

    Clean up weird characters

--- twitter_test.py
+++ twitter_test.py
@@ -1,6 +1,46 @@
 import tweepy
 import csv
 
+LATIN_1_CHARS = (
+    ('\xe2\x80\x99', "'"),
+    ('\xc3\xa9', 'e'),
+    ('\xe2\x80\x90', '-'),
+    ('\xe2\x80\x91', '-'),
+    ('\xe2\x80\x92', '-'),
+    ('\xe2\x80\x93', '-'),
+    ('\xe2\x80\x94', '-'),
+    ('\xe2\x80\x94', '-'),
+    ('\xe2\x80\x98', "'"),
+    ('\xe2\x80\x9b', "'"),
+    ('\xe2\x80\x9c', '"'),
+    ('\xe2\x80\x9c', '"'),
+    ('\xe2\x80\x9d', '"'),
+    ('\xe2\x80\x9e', '"'),
+    ('\xe2\x80\x9f', '"'),
+    ('\xe2\x80\xa6', '...'),
+    ('\xe2\x80\xb2', "'"),
+    ('\xe2\x80\xb3', "'"),
+    ('\xe2\x80\xb4', "'"),
+    ('\xe2\x80\xb5', "'"),
+    ('\xe2\x80\xb6', "'"),
+    ('\xe2\x80\xb7', "'"),
+    ('\xe2\x81\xba', "+"),
+    ('\xe2\x81\xbb', "-"),
+    ('\xe2\x81\xbc', "="),
+    ('\xe2\x81\xbd', "("),
+    ('\xe2\x81\xbe', ")")
+)
+
+def clean_latin1(data):
+    try:
+        return data.encode('utf-8')
+    except UnicodeDecodeError:
+        data = data.decode('iso-8859-1')
+        for _hex, _char in LATIN_1_CHARS:
+            data = data.replace(_hex, _char)
+        return data.encode('utf8')
+
+
 consumer_key = 'TErus3u1UOUgdR6AKhIaaE9Wg'
 consumer_secret = 'UI8yW0epbpuX9CxNl91eSVCGtmDh9IBkUvopH2TvPIaLjVgOCy'
 access_token = '1234781863597760512-SAmWryVIvcqjHM44kTbHhTS0SFExu6'
@@ -11,11 +51,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 hashtag = 'coronavirus'
-csvFile = open(f'{hashtag}.csv', 'w', encoding='utf-8')
-csvWriter = csv.writer(csvFile)
-
-for tweet in tweepy.Cursor(api.search,q=f"#{hashtag}",count=100,
+for tweet in tweepy.Cursor(api.search,q=f"#{hashtag}",
                            lang="en",
-                           since="2017-04-03").items():
-    print (tweet.created_at, tweet.text)
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+                           since="2019-12-01").items(10):
+    print(tweet.text)
